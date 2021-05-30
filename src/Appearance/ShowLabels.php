6de889ea5ba141afc2e8dd812c6cd8448d7125c3
Maarten Paauw <maartenpaauw@gmail.com>
2021-05-30 10:13:53 +0200

    chore: add show labels modification

---
+++ src/Appearance/ShowLabels.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Maartenpaauw\Chart\Appearance;
+
+class ShowLabels implements Modification
+{
+    public function classes(): array
+    {
+        return ['show-labels'];
+    }
+}
