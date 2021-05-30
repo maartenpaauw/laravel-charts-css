30f3ff30c052e091fc8439b87433aabccec8bf41
maartenpaauw <maartenpaauw@users.noreply.github.com>
2021-05-30 08:35:46 +0000

    fix: apply php cs fixer changes

--- tests/Configuration/Specifications/HasHeadingTest.php
+++ tests/Configuration/Specifications/HasHeadingTest.php
@@ -9,7 +9,7 @@ use Maartenpaauw\Chart\Tests\TestCase;
 class HasHeadingTest extends TestCase
 {
     /** @test */
-    public function it_should_return_true_when_the_given_configuration_has_a_heading    (): void
+    public function it_should_return_true_when_the_given_configuration_has_a_heading(): void
     {
         // Arrange
         $configuration = new Configuration();
@@ -21,5 +21,4 @@ class HasHeadingTest extends TestCase
         // Assert
         $this->assertTrue($satisfied);
     }
-
 }
