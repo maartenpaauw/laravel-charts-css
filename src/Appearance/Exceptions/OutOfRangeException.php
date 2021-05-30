7bfd3855e53b4c831c585c71a82874233bd5e44a
Maarten Paauw <maartenpaauw@gmail.com>
2021-05-30 16:22:30 +0200

    chore: add out of range exception

---
+++ src/Appearance/Exceptions/OutOfRangeException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Maartenpaauw\Chart\Appearance\Exceptions;
+
+use InvalidArgumentException;
+
+class OutOfRangeException extends InvalidArgumentException
+{
+    public function __construct(int $axes, int $start, int $end)
+    {
+        parent::__construct("The given axes amount `{$axes}` should be between {$start} and {$end}.");
+    }
+}
