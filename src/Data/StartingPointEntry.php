d8c7cbd89cfe1ae5f753e5bae498d7d1457d3a32
Maarten Paauw <maartenpaauw@gmail.com>
2021-05-31 23:09:25 +0200

    feat(entry): add starting point entry

---
+++ src/Data/StartingPointEntry.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Maartenpaauw\Chart\Data;
+
+class StartingPointEntry implements EntryContract
+{
+    private EntryContract $origin;
+
+    private EntryContract $previous;
+
+    public function __construct(EntryContract $origin, EntryContract $previous)
+    {
+        $this->origin = $origin;
+        $this->previous = $previous;
+    }
+
+    public function value(): string
+    {
+        return $this->origin->value();
+    }
+
+    public function raw(): float
+    {
+        return $this->origin->raw();
+    }
+
+    public function start(): float
+    {
+        return $this->previous->raw();
+    }
+}
---
+++ tests/Data/StartingPointEntryTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Maartenpaauw\Chart\Tests\Data;
+
+use Maartenpaauw\Chart\Data\Entry;
+use Maartenpaauw\Chart\Data\EntryContract;
+use Maartenpaauw\Chart\Data\StartingPointEntry;
+use Maartenpaauw\Chart\Tests\TestCase;
+
+class StartingPointEntryTest extends TestCase
+{
+    private EntryContract $origin;
+
+    private EntryContract $previous;
+
+    private EntryContract $entry;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->origin = new Entry('10', 10);
+        $this->previous = new Entry('20', 20);
+        $this->entry = new StartingPointEntry($this->origin, $this->previous);
+    }
+
+    /** @test */
+    public function it_should_return_the_previous_raw_value_as_start_value(): void
+    {
+        $this->assertEquals($this->previous->raw(), $this->entry->start());
+    }
+
+    /** @test */
+    public function it_should_return_the_origin_value(): void
+    {
+        $this->assertEquals($this->origin->value(), $this->entry->value());
+    }
+
+    /** @test */
+    public function it_should_return_the_origin_raw_value(): void
+    {
+        $this->assertEquals($this->origin->raw(), $this->entry->raw());
+    }
+}
