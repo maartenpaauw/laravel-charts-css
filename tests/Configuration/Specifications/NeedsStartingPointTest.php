703d6b6c4d159fdcb4662379f722a1b42017766e
Maarten Paauw <maartenpaauw@gmail.com>
2021-06-08 09:58:19 +0200

    chore: add test for needs starting point specification

---
+++ tests/Configuration/Specifications/NeedsStartingPointTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Maartenpaauw\Chart\Tests\Configuration\Specifications;
+
+use Maartenpaauw\Chart\Appearance\Colorscheme\Colorscheme;
+use Maartenpaauw\Chart\Appearance\Colorscheme\ColorschemeContract;
+use Maartenpaauw\Chart\Appearance\ModificationsBag;
+use Maartenpaauw\Chart\Configuration\Configuration;
+use Maartenpaauw\Chart\Configuration\Specifications\NeedsStartingPoint;
+use Maartenpaauw\Chart\Identity\Identity;
+use Maartenpaauw\Chart\Legend\Legend;
+use Maartenpaauw\Chart\Tests\TestCase;
+use Maartenpaauw\Chart\Types\Area;
+use Maartenpaauw\Chart\Types\Bar;
+use Maartenpaauw\Chart\Types\Column;
+use Maartenpaauw\Chart\Types\Line;
+
+class NeedsStartingPointTest extends TestCase
+{
+    private NeedsStartingPoint $needsStartingPoint;
+
+    private Legend $legend;
+
+    private ModificationsBag $modifications;
+
+    private ColorschemeContract $colorscheme;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->needsStartingPoint = new NeedsStartingPoint();
+        $this->legend = new Legend();
+        $this->modifications = new ModificationsBag();
+        $this->colorscheme = new Colorscheme();
+    }
+
+    /** @test */
+    public function it_should_return_true_when_the_chart_type_is_area(): void
+    {
+        // Arrange
+        $identity = new Identity('', '', new Area());
+
+        $configuration = new Configuration(
+            $identity,
+            $this->legend,
+            $this->modifications,
+            $this->colorscheme,
+        );
+
+        // Act
+        $satisfied = $this->needsStartingPoint->isSatisfiedBy($configuration);
+
+        // Assert
+        $this->assertTrue($satisfied);
+    }
+
+    /** @test */
+    public function it_should_return_true_when_the_chart_type_is_line(): void
+    {
+        // Arrange
+        $identity = new Identity('', '', new Line());
+
+        $configuration = new Configuration(
+            $identity,
+            $this->legend,
+            $this->modifications,
+            $this->colorscheme,
+        );
+
+        // Act
+        $satisfied = $this->needsStartingPoint->isSatisfiedBy($configuration);
+
+        // Assert
+        $this->assertTrue($satisfied);
+    }
+
+    /** @test */
+    public function it_should_return_false_when_the_chart_type_is_bar(): void
+    {
+        // Arrange
+        $identity = new Identity('', '', new Bar());
+
+        $configuration = new Configuration(
+            $identity,
+            $this->legend,
+            $this->modifications,
+            $this->colorscheme,
+        );
+
+        // Act
+        $satisfied = $this->needsStartingPoint->isSatisfiedBy($configuration);
+
+        // Assert
+        $this->assertFalse($satisfied);
+    }
+
+    /** @test */
+    public function it_should_return_false_when_the_chart_type_is_column(): void
+    {
+        // Arrange
+        $identity = new Identity('', '', new Column());
+
+        $configuration = new Configuration(
+            $identity,
+            $this->legend,
+            $this->modifications,
+            $this->colorscheme,
+        );
+
+        // Act
+        $satisfied = $this->needsStartingPoint->isSatisfiedBy($configuration);
+
+        // Assert
+        $this->assertFalse($satisfied);
+    }
+}
