aaf847e2ca0dac0e8c1a71a9aa245497f5ae749e
Maarten Paauw <maartenpaauw@gmail.com>
2021-06-21 12:09:44 +0200

    docs: add data example 5

---
+++ src/Examples/Data/DataExample5.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Maartenpaauw\Chart\Examples\Data;
+
+use Maartenpaauw\Chart\Appearance\ModificationsBag;
+use Maartenpaauw\Chart\Appearance\ShowDataOnHover;
+use Maartenpaauw\Chart\Chart;
+use Maartenpaauw\Chart\Configuration\Configuration;
+use Maartenpaauw\Chart\Configuration\ConfigurationContract;
+use Maartenpaauw\Chart\Data\Axes\NullAxes;
+use Maartenpaauw\Chart\Data\Datasets\Dataset;
+use Maartenpaauw\Chart\Data\Datasets\Datasets;
+use Maartenpaauw\Chart\Data\Datasets\DatasetsContract;
+use Maartenpaauw\Chart\Data\Entries\Entry;
+use Maartenpaauw\Chart\Data\Entries\Value\Value;
+
+class DataExample5 extends Chart
+{
+    protected function id(): string
+    {
+        return 'data-example-5';
+    }
+
+    protected function heading(): string
+    {
+        return 'Data Example #5 - Front End Developer Salary';
+    }
+
+    protected function datasets(): DatasetsContract
+    {
+        return new Datasets(
+            new NullAxes(),
+            [
+                new Dataset([
+                    new Entry(new Value(40, '$40k')),
+                    new Entry(new Value(60, '$60k')),
+                    new Entry(new Value(75, '$75k')),
+                    new Entry(new Value(90, '$90k')),
+                    new Entry(new Value(100, '$100k')),
+                ]),
+            ],
+        );
+    }
+
+    protected function modifications(): ModificationsBag
+    {
+        return parent::modifications()
+            ->add(new ShowDataOnHover());
+    }
+
+    protected function configuration(): ConfigurationContract
+    {
+        return new Configuration(
+            $this->identity(),
+            $this->legend(),
+            $this->modifications(),
+            $this->colorscheme(),
+        );
+    }
+}
