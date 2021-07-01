43f59e208771e4c11ac4f4c91cb4728a8f92eb42
Maarten Paauw <maartenpaauw@gmail.com>
2021-07-01 22:58:13 +0200

    docs: add labels example 6

---
+++ src/Examples/Labels/LabelsExample6.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Maartenpaauw\Chart\Examples\Labels;
+
+use Maartenpaauw\Chart\Appearance\HideData;
+use Maartenpaauw\Chart\Appearance\LabelsAlignEnd;
+use Maartenpaauw\Chart\Appearance\Modifications;
+use Maartenpaauw\Chart\Appearance\Multiple;
+use Maartenpaauw\Chart\Appearance\ShowLabels;
+use Maartenpaauw\Chart\Chart;
+use Maartenpaauw\Chart\Configuration\Configuration;
+use Maartenpaauw\Chart\Configuration\ConfigurationContract;
+use Maartenpaauw\Chart\Data\Axes\Axes;
+use Maartenpaauw\Chart\Data\Datasets\Dataset;
+use Maartenpaauw\Chart\Data\Datasets\Datasets;
+use Maartenpaauw\Chart\Data\Datasets\DatasetsContract;
+use Maartenpaauw\Chart\Data\Entries\Entry;
+use Maartenpaauw\Chart\Data\Entries\Value\Value;
+use Maartenpaauw\Chart\Data\Label\Label;
+
+class LabelsExample6 extends Chart
+{
+    protected function id(): string
+    {
+        return 'labels-example-6';
+    }
+
+    protected function heading(): string
+    {
+        return 'Label Example #6';
+    }
+
+    protected function datasets(): DatasetsContract
+    {
+        return new Datasets(
+            new Axes('Year', [
+                'Progress 1',
+                'Progress 2',
+                'Progress 3',
+                'Progress 4',
+                'Progress 5',
+            ]),
+            new Dataset([
+                new Entry(new Value(10)),
+                new Entry(new Value(30)),
+                new Entry(new Value(50)),
+                new Entry(new Value(70)),
+                new Entry(new Value(90)),
+            ], new Label('2010')),
+            new Dataset([
+                new Entry(new Value(20)),
+                new Entry(new Value(40)),
+                new Entry(new Value(60)),
+                new Entry(new Value(80)),
+                new Entry(new Value(100)),
+            ], new Label('2020')),
+        );
+    }
+
+    protected function configuration(): ConfigurationContract
+    {
+        return new Configuration(
+            $this->identity(),
+            $this->legend(),
+            $this->modifications(),
+            $this->colorscheme(),
+        );
+    }
+
+    protected function modifications(): Modifications
+    {
+        return parent::modifications()
+            ->add(new Multiple())
+            ->add(new ShowLabels())
+            ->add(new HideData())
+            ->add(new LabelsAlignEnd());
+    }
+}
