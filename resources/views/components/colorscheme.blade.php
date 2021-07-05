0bf89a1a77552e50ba01a6a20c5b9db80755ae28
Maarten Paauw <maartenpaauw@gmail.com>
2021-07-05 10:53:14 +0200

    refactor: remove once wrapper
    This will introduce multiple style declarations if a chart is rendered multiple times. We need to fix this in the future.

--- resources/views/components/colorscheme.blade.php
+++ resources/views/components/colorscheme.blade.php
@@ -1,9 +1,7 @@
-@once
-    @push('charts.css')
-        @hasLabels($configuration) {{ $id }} ul, {{ $id }} table @else {{ $id }} @endhasLabels {
-            @foreach($declarations as $declaration)
-                {!! $declaration !!}
-            @endforeach
-        }
-    @endpush
-@endonce
+@push('charts.css')
+    @hasLabels($configuration) {{ $id }} ul, {{ $id }} table @else {{ $id }} @endhasLabels {
+        @foreach($declarations as $declaration)
+            {!! $declaration !!}
+        @endforeach
+    }
+@endpush
