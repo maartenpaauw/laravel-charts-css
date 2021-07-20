9d20a6607f93a4655d696ab87bb0c8d6a28b0470
Maarten Paauw <maartenpaauw@gmail.com>
2021-07-20 22:44:43 +0200

    work in progress

--- src/Commands/MakeChartCommand.php
+++ src/Commands/MakeChartCommand.php
@@ -4,6 +4,7 @@ namespace Maartenpaauw\Chartscss\Commands;
 
 use Illuminate\Console\GeneratorCommand;
 use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputOption;
 
 class MakeChartCommand extends GeneratorCommand
 {
@@ -28,8 +29,8 @@ class MakeChartCommand extends GeneratorCommand
     protected function replaceClass($stub, $name): string
     {
         return Str::replace(
-            ['dummy-id', 'Dummy Heading'],
-            [$this->id(), $this->heading()],
+            ['dummy-id', 'Dummy Heading', 'DummyType'],
+            [$this->id(), $this->heading(), $this->type()],
             parent::replaceClass($stub, $name),
         );
     }
@@ -52,5 +53,21 @@ class MakeChartCommand extends GeneratorCommand
     private function isCustomNamespace(): bool
     {
         return Str::contains($this->getNameInput(), '/');
+    }
+
+    public function type(): string
+    {
+        if ($this->option('type') === 'column') {
+            return 'Chart';
+        }
+
+        return sprintf('%sChart', ucfirst($this->option('type')));
+    }
+
+    protected function getOptions(): array
+    {
+        return [
+            ['type', 't', InputOption::VALUE_OPTIONAL, 'The chart type to use', 'column'],
+        ];
     }
 }
