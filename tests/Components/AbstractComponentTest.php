6056cc3a3eb893f307d5dcf47ce13b65de58274c
Maarten Paauw <maartenpaauw@gmail.com>
2021-10-26 08:35:22 +0200

    test(components): add heading component test

---
+++ tests/Components/AbstractComponentTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Maartenpaauw\Chartscss\Tests\Components;
+
+use Illuminate\View\Component;
+use Maartenpaauw\Chartscss\Tests\Snapshot\Driver\CustomHtmlDriver;
+use Maartenpaauw\Chartscss\Tests\Snapshot\TestCase;
+
+abstract class AbstractComponentTest extends TestCase
+{
+    abstract protected function component(): Component;
+
+    /** @test */
+    public function it_should_render_the_component_correctly(): void
+    {
+        // Act
+        $render = $this->component()->render();
+
+        if (! is_string($render)) {
+            $render = $render->toHtml();
+        }
+
+        // Assert
+        $this->assertMatchesSnapshot(
+            $render,
+            new CustomHtmlDriver(),
+        );
+    }
+
+}
---
+++ tests/Components/HeadingTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Maartenpaauw\Chartscss\Tests\Components;
+
+use Illuminate\View\Component;
+use Maartenpaauw\Chartscss\Components\Heading;
+
+class HeadingTest extends AbstractComponentTest
+{
+    protected function component(): Component
+    {
+        return new Heading('Hello world!');
+    }
+}
